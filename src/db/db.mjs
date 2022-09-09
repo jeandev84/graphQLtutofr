6972ffb73e2dd2f2558c312749c08d5278c9442c
jeandev84 <jeandev84@yahoo.com>
2022-09-09 03:51:14 +0300

    GraphQL Query avec arguments

---
+++ src/db/db.mjs
@@ -0,0 +1,16 @@
+export const db =  {
+    todos: [
+        {
+            id: 1,
+            name: "Apprendre GraphQL",
+            content: "Entamer la partie Query",
+            status: 'WAITING'
+        },
+        {
+            id: 2,
+            name: "Apprendre GraphQL",
+            content: "Entamer la partie Schema",
+            status: 'DONE'
+        }
+    ]
+}
